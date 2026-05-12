c3a03e617f491f8b5c6296f706172005a1101b01
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-12 13:29:32 +0530

    ..

--- final/script.js
+++ final/script.js
@@ -1,30 +1,32 @@
 import express from "express"
 import mongoose from "mongoose"
-import dotent from "dotenv"
+import dotenv from "dotenv"
 import authRouter from "./routers/authRouter.js"
 import notesRouter from "./routers/notesRouter.js"
 
-dotent.config()
-
-
-mongoose.connect(process.env.MONGODB_STRING).then(() => {
-    console.log("DB Connected Successfully");
-}).catch((err) => {
-    console.log(err)
-})
+dotenv.config()
 
 const app = express()
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
 
-app.get("/",(req,res)=>{
-    res.status(200).json({message:"API is running!"})
+mongoose.connect(process.env.MONGODB_STRING)
+.then(() => {
+    console.log("DB Connected Successfully")
 })
-// req Handling 
-app.use("/api/auth", authRouter)
-app.use("/api/notes",notesRouter)
+.catch((err) => {
+    console.log(err)
+})
 
-app.listen(3333, () => {
-    console.log("Listening on 3333")
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "API is running!"
+    })
 })
+
+app.use("/api/auth", authRouter)
+app.use("/api/notes", notesRouter)
+
+// IMPORTANT FOR VERCEL
+export default app
