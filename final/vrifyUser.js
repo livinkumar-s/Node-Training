e018ba461a1d3e751d7ab86a63bbf7df88bdb987
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-13 13:50:07 +0530

    ..

--- final/vrifyUser.js
+++ final/vrifyUser.js
@@ -3,7 +3,7 @@ import jsonwebtoken from "jsonwebtoken"
 const verifyUser = async (req, res, next) => {
     let token;
     if (req.cookies?.auth) token = req.cookies?.auth;
-    if (req.headers?.authorization?.startsWith("Bearer")) token=req.headers?.authorization.split(" ")[1]
+    else if (req.headers?.authorization?.startsWith("Bearer")) token=req.headers?.authorization.split(" ")[1]
     
     console.log(token);
     
