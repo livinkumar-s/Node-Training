37bbe14a60bbef1d569420d9d1ae7ab8ce4a603b
LivinkumarStackly <livinkumar@thestackly.com>
2026-05-13 13:55:37 +0530

    ..

--- final/vrifyUser.js
+++ final/vrifyUser.js
@@ -7,7 +7,6 @@ const verifyUser = async (req, res, next) => {
     else if (req.headers?.authorization?.startsWith("Bearer")) token=req.headers?.authorization.split(" ")[1]
     
     console.log(token);
-    
 
     if (!token) {
         return res.status(401).json({
